fb1a654152094a325433efdad598de7b36e37b05
Isabellafsr <143718574+Isabellafsr@users.noreply.github.com>
2023-11-27 20:44:11 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack'
+import { useNavigation } from "@react-navigation/native";
 
-export default function App() {
+
+function cadastrar() {
+  const navigation = useNavigation()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Text>Seja bem vindo!</Text><Button />
+      <TextInput/>
+      <TextInput secureTextEntry={true}
+      />
+      <Button title="Cadastrar" onPress={() => {
+        navigation.navigate("iniciologado")
+      }}/>
+      <Button title="fazer login"
+      onPress={() => {
+        navigation.navigate("telalogin")
+
+      }}/>
     </View>
-  );
+    
+  )
+}
+function iniciologado() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>parabens pelo cadastro</Text>
+      <Button title="Voltar"
+      onPress={() => {
+        navigation.navigate("cadastrar")
+
+      }}/>
+      <Button title="go perfil"
+      onPress={() => {
+        navigation.navigate("profile")
+      }}/>
+      <Button title="suporte"
+      onPress={() => {
+        navigation.navigate("chat")
+      }}/>
+    </View>
+  )
+}
+function home() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>Bem vindo</Text>
+      <Button title="Cadastrar"
+      onPress={() => {
+        navigation.navigate("cadastrar")
+      }}/>
+    </View>
+  )
+  }
+function login() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>Faça o login</Text>
+      <Button title="login"
+      onPress={() => {
+        navigation.navigate("iniciologado")
+      }}/>
+    </View>
+  )
+}
+function profile() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>seu perfil</Text>
+      <Button title="Voltar"
+      onPress={() => {
+        navigation.navigate("iniciologado")
+      }}/>
+    </View>
+  )
+  
+}
+function chat() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>Papo</Text>
+      <Button title="Voltar"
+      onPress={() => {
+        navigation.navigate("iniciologado")
+      }}/>
+    </View>
+  )
+  
+}
+export default function App() {
+
+  const Stack = createStackNavigator()
+  return (
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName="home"> 
+        <Stack.Screen name="cadastrar" component={cadastrar} />
+        <Stack.Screen name="iniciologado" component={iniciologado} />
+        <Stack.Screen name="home" component={home} />
+        <Stack.Screen name="login" component={login} />
+        <Stack.Screen name="profile" component={profile} />
+        <Stack.Screen name="chat" component={chat} />
+
+       </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
